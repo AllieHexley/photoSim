6b8ae63f0dd2e1ed062b70785a2f5c2fc0112b24
Allie Hexley <allie@Allies-MacBook-Pro-10.local>
2020-04-10 14:55:13 +0100

    updates

--- getSimulatedResponses.m
+++ getSimulatedResponses.m
@@ -13,12 +13,35 @@ spd = spd(3:end,2:end);
 
 % load reflectance spectra from the TM-30-15 standard
 ref = csvread('99Reflectances.csv');
-wlsRef = ref(:,1);
+wlsRef = ref(11:5:401,1);
 % resample to match the spectra and cone fundamentals space
 ref = ref(11:5:401,2:end);
 
 % normalise illuminant spectra
 normSpd = normIllSpd(spd, wlsSpd);
+
+% calculate simulated radiant spectra
+k=1;
+for i=1:size(normSpd,2)
+    for j = 1:size(ref,2)
+        rad(:,k) = (ref(:,j).*normSpd(:,i));
+        k=k+1;
+    end
+end
+
+% check norm is correct by summing integrals of k, should equal total
+% number of radiance spectra (39699)
+normCheck=sum(trapz(wlsSpd, rad(:,:)));
+for i=1:size(rad,2)
+    if trapz(wlsSpd, rad(:,i)) == 1
+        normFails(i) = 0;
+    elseif trapz(wlsSpd, rad(:,i)) > 1
+        normFails(i) = 1;
+    else 
+        normFails(i) = -1;
+    end
+end
+% the integrals of all the radiance spectra are below 1
 
 % calculate L,M,S,R,I responses
 for i=1:size(normSpd,2)
--- getSpectralLocus.m
+++ getSpectralLocus.m
@@ -7,7 +7,13 @@ function spectralLocus = getSpectralLocus(activations)
 
 slSPD = zeros(size(activations,2),size(activations,2));
 for ii=1:size(activations,2)
+    if ii==1
+        slSPD(ii,ii) = 1;
+    elseif ii==size(activations,2)
+        slSPD(ii,ii) = 1;
+    else
     slSPD(ii,ii) = 1;
+    end
 end;
 
 % calculate spectral locus in L,M,S,R,I coordinate space
