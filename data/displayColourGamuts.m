95ed789bfa8b02f2a75dcf375d917cb79b029992
Allie Hexley <allie@Allies-MacBook-Pro-10.local>
2020-05-26 12:51:05 +0100

    updates

--- data/displayColourGamuts.m
+++ data/displayColourGamuts.m
@@ -2,6 +2,9 @@
 % MacLeod-Boynton space to see impact of melanopsin on gamut shape
 % created by ACH on 13/05/2020
 
+clear all;
+clc;
+
 %% Set up colorimetry
 % Get the CIE 2015 10degree XYZ functions
 T_xyz = csvread('lin2012xyz10e_1_7sf.csv');
@@ -181,169 +184,10 @@ xyYCRTcombo(isnan(xyYCRTcombo)) = 0;
 xyYLCDcombo(isnan(xyYLCDcombo)) = 0;
 xyYCDPcombo(isnan(xyYDPcombo)) = 0;
 
-%% plot heatmaps for photopic, scoptopic luminance, and melanopsin
-figure('defaultAxesFontSize',18)
+%% get spectral and daylight locus
 
-% xyY 
-subplot(3,3,1)
-scatter(xyYCRTcombo(1,:),xyYCRTcombo(2,:),[],ssCRTcombo(2,:)+ssCRTcombo(3,:),'filled');
-c = colorbar
-c.Label.String = 'L+M';
-xlim([0,1]);
-ylim([0,1]);
-title('CRT');
-xlabel('x');
-ylabel('y');
-
-
-subplot(3,3,2)
-scatter(xyYLCDcombo(1,:),xyYLCDcombo(2,:),[],ssLCDcombo(2,:)+ssLCDcombo(3,:),'filled');
-c = colorbar
-c.Label.String = 'L+M';
-xlim([0,1]);
-ylim([0,1]);
-title('LCD');
-xlabel('x');
-ylabel('y');
-
-
-subplot(3,3,3)
-scatter(xyYDPcombo(1,:),xyYDPcombo(2,:),[],ssDPcombo(2,:)+ssDPcombo(3,:),'filled');
-c = colorbar
-c.Label.String = 'L+M';
-xlim([0,1]);
-ylim([0,1]);
-title('Display++');
-xlabel('x');
-ylabel('y');
-
-% xyMel
-subplot(3,3,4)
-scatter(xyYCRTcombo(1,:),xyYCRTcombo(2,:),[],ssCRTcombo(5,:),'filled');
-c = colorbar
-c.Label.String = 'Mel';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-ylabel('y');
-
-subplot(3,3,5)
-scatter(xyYLCDcombo(1,:),xyYLCDcombo(2,:),[],ssLCDcombo(5,:),'filled');
-c = colorbar
-c.Label.String = 'Mel';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-ylabel('y');
-
-subplot(3,3,6)
-scatter(xyYDPcombo(1,:),xyYDPcombo(2,:),[],ssDPcombo(5,:),'filled');
-c = colorbar
-c.Label.String = 'Mel';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-ylabel('y');
-zlabel('Y');
-
-% xyRod
-subplot(3,3,7)
-scatter(xyYCRTcombo(1,:),xyYCRTcombo(2,:),[],ssCRTcombo(4,:),'filled');
-c = colorbar
-c.Label.String = 'Rod';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-ylabel('y');
-
-subplot(3,3,8)
-scatter(xyYLCDcombo(1,:),xyYLCDcombo(2,:),[],ssLCDcombo(4,:),'filled');
-c = colorbar
-c.Label.String = 'Rod';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-ylabel('y');
-
-subplot(3,3,9)
-scatter(xyYDPcombo(1,:),xyYDPcombo(2,:),[],ssDPcombo(4,:),'filled');
-c = colorbar
-c.Label.String = 'Rod';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-ylabel('y');
-zlabel('Y');
-
-%% plot difference between lum and mel heatmaps
-figure('defaultAxesFontSize',18)
-
-subplot(1,3,1)
-scatter(xyYCRTcombo(1,:),xyYCRTcombo(2,:),[],(ssCRTcombo(2,:)+ssCRTcombo(3,:))-ssCRTcombo(5,:),'filled');
-c = colorbar
-c.Label.String = 'Lum-Mel';
-xlim([0,1]);
-ylim([0,1]);
-title('CRT');
-xlabel('x');
-ylabel('y');
-
-
-subplot(1,3,2)
-scatter(xyYLCDcombo(1,:),xyYLCDcombo(2,:),[],(ssLCDcombo(2,:)+ssLCDcombo(3,:))-ssLCDcombo(5,:),'filled');
-c = colorbar
-c.Label.String = 'Lum-Mel';
-xlim([0,1]);
-ylim([0,1]);
-title('LCD');
-xlabel('x');
-ylabel('y');
-
-
-subplot(1,3,3)
-scatter(xyYDPcombo(1,:),xyYDPcombo(2,:),[],(ssDPcombo(2,:)+ssDPcombo(3,:))-ssDPcombo(5,:),'filled');
-c = colorbar
-c.Label.String = 'Lum-Mel';
-xlim([0,1]);
-ylim([0,1]);
-title('Display++');
-xlabel('x');
-ylabel('y');
-
-%% plot as mel/lum 
-figure('defaultAxesFontSize',18)
-
-subplot(1,3,1)
-scatter(xyYCRTcombo(1,:),xyYCRTcombo(2,:),[],ssCRTcombo(5,:)./(ssCRTcombo(2,:)+ssCRTcombo(3,:)),'filled');
-c = colorbar
-c.Label.String = 'Mel/Lum';
-xlim([0,1]);
-ylim([0,1]);
-title('CRT');
-xlabel('x');
-ylabel('y');
-
-
-subplot(1,3,2)
-scatter(xyYLCDcombo(1,:),xyYLCDcombo(2,:),[],ssLCDcombo(5,:)./(ssLCDcombo(2,:)+ssLCDcombo(3,:)),'filled');
-c = colorbar
-c.Label.String = 'Mel/Lum';
-xlim([0,1]);
-ylim([0,1]);
-title('LCD');
-xlabel('x');
-ylabel('y');
-
-
-subplot(1,3,3)
-scatter(xyYDPcombo(1,:),xyYDPcombo(2,:),[],ssDPcombo(5,:)./(ssDPcombo(2,:)+ssDPcombo(3,:)),'filled');
-c = colorbar
-c.Label.String = 'Mel/Lum';
-xlim([0,1]);
-ylim([0,1]);
-title('Display++');
-xlabel('x');
-ylabel('y');
+slRad = getSpectralLocusSpectra(390:780);
+dlRad = getDaylightSpectra;
 
 %% get simulated radiant spectra
 
@@ -357,22 +201,286 @@ wls_xyz = wls_xyz(wls_xyz >= 390 & wls_xyz <= 780, 1);
 wls_xyz_5nm = wls_xyz(1:5:end);
 T_xyz_5nm = T_xyz(:,1:5:end);
 
-[T_cies026, S_cies026] = GetCIES026;
-wlsCIES026 = (390:1:780)';
 % rescale only over range where we have cone fundmanetals i.e. 390nm:780nm
 % scale for spds with 5nm spacing
 wls_cies026_5nm = wlsCIES026(1:5:end);
-T_cies026_5nm = T_cies026(:,11:5:end);
+T_cies026_5nm = T_cies026(:,1:5:end);
 % remove Nans
 T_cies026_5nm(isnan(T_cies026_5nm)) = 0;
 
+%% calculate xyY and photoreceptor activations of spectral locus
+
+% calculate xyY coordinates of spectral locus
+xyYSL = XYZToxyY(T_xyz*slRad);
+idxSL = convhull(xyYSL(1,:), xyYSL(2,:));
+
+% calculate photoreceptor activations of spectral locus
+ssSL = T_cies026*slRad;
+
 %% calculate xyY and photoreceptor activations of simulated spectra
 
-% calculate xyY coordinates of simulated spectra
+% calculate xyY coordinates of daylight locus
+xyYDL = XYZToxyY(T_xyz_5nm*dlRad);
+idxDL = convhull(xyYDL(1,:), xyYDL(2,:));
+
+% calculate photoreceptor activations of daylight locus
+ssDL = T_cies026_5nm*dlRad;
+
+%% calculate xyY and photoreceptor activations of simulated spectra
+
+% calculate xyY coordinates of simulated
 xyYSim = XYZToxyY(T_xyz_5nm*simRad);
 
-% calculate photoreceptor activations of simulated spectra
+% calculate photoreceptor activations of simulated
 ssSim = T_cies026_5nm*simRad;
+
+%% plot heatmaps for photopic, scoptopic luminance, and melanopsin
+figure('defaultAxesFontSize',18)
+
+% xyY 
+subplot(3,4,1)
+scatter(xyYCRTcombo(1,:),xyYCRTcombo(2,:),[],ssCRTcombo(2,:)+ssCRTcombo(3,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'L+M';
+xlim([0,1]);
+ylim([0,1]);
+title('CRT');
+xlabel('x');
+ylabel('y');
+
+
+subplot(3,4,2)
+scatter(xyYLCDcombo(1,:),xyYLCDcombo(2,:),[],ssLCDcombo(2,:)+ssLCDcombo(3,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'L+M';
+xlim([0,1]);
+ylim([0,1]);
+title('LCD');
+xlabel('x');
+ylabel('y');
+
+
+subplot(3,4,3)
+scatter(xyYDPcombo(1,:),xyYDPcombo(2,:),[],ssDPcombo(2,:)+ssDPcombo(3,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'L+M';
+xlim([0,1]);
+ylim([0,1]);
+title('Display++');
+xlabel('x');
+ylabel('y');
+
+subplot(3,4,4)
+scatter(xyYSim(1,:),xyYSim(2,:),5,(ssSim(2,:)+ssSim(3,:)),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'L+M';
+xlim([0,1]);
+ylim([0,1]);
+title('Simulated Spectra');
+xlabel('x');
+ylabel('y');
+
+% xyMel
+subplot(3,4,5)
+scatter(xyYCRTcombo(1,:),xyYCRTcombo(2,:),[],ssCRTcombo(5,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Mel';
+xlim([0,1]);
+ylim([0,1]);
+xlabel('x');
+ylabel('y');
+
+subplot(3,4,6)
+scatter(xyYLCDcombo(1,:),xyYLCDcombo(2,:),[],ssLCDcombo(5,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Mel';
+xlim([0,1]);
+ylim([0,1]);
+xlabel('x');
+ylabel('y');
+
+subplot(3,4,7)
+scatter(xyYDPcombo(1,:),xyYDPcombo(2,:),[],ssDPcombo(5,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Mel';
+xlim([0,1]);
+ylim([0,1]);
+xlabel('x');
+ylabel('y');
+zlabel('Y');
+
+subplot(3,4,8)
+scatter(xyYSim(1,:),xyYSim(2,:),5,(ssSim(5,:)),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Mel';
+xlim([0,1]);
+ylim([0,1]);
+xlabel('x');
+ylabel('y');
+
+% xyRod
+subplot(3,4,9)
+scatter(xyYCRTcombo(1,:),xyYCRTcombo(2,:),[],ssCRTcombo(4,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Rod';
+xlim([0,1]);
+ylim([0,1]);
+xlabel('x');
+ylabel('y');
+
+subplot(3,4,10)
+scatter(xyYLCDcombo(1,:),xyYLCDcombo(2,:),[],ssLCDcombo(4,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Rod';
+xlim([0,1]);
+ylim([0,1]);
+xlabel('x');
+ylabel('y');
+
+subplot(3,4,11)
+scatter(xyYDPcombo(1,:),xyYDPcombo(2,:),[],ssDPcombo(4,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Rod';
+xlim([0,1]);
+ylim([0,1]);
+xlabel('x');
+ylabel('y');
+zlabel('Y');
+
+subplot(3,4,12)
+scatter(xyYSim(1,:),xyYSim(2,:),5,(ssSim(4,:)),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Rod';
+xlim([0,1]);
+ylim([0,1]);
+xlabel('x');
+ylabel('y');
+
+
+%% plot difference between lum and mel heatmaps
+figure('defaultAxesFontSize',18)
+
+subplot(2,2,1)
+scatter(xyYCRTcombo(1,:),xyYCRTcombo(2,:),[],(ssCRTcombo(2,:)+ssCRTcombo(3,:))-ssCRTcombo(5,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Lum-Mel';
+xlim([0,1]);
+ylim([0,1]);
+title('CRT');
+xlabel('x');
+ylabel('y');
+
+
+subplot(2,2,2)
+scatter(xyYLCDcombo(1,:),xyYLCDcombo(2,:),[],(ssLCDcombo(2,:)+ssLCDcombo(3,:))-ssLCDcombo(5,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Lum-Mel';
+xlim([0,1]);
+ylim([0,1]);
+title('LCD');
+xlabel('x');
+ylabel('y');
+
+
+subplot(2,2,3)
+scatter(xyYDPcombo(1,:),xyYDPcombo(2,:),[],(ssDPcombo(2,:)+ssDPcombo(3,:))-ssDPcombo(5,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Lum-Mel';
+xlim([0,1]);
+ylim([0,1]);
+title('Display++');
+xlabel('x');
+ylabel('y');
+
+subplot(2,2,4)
+scatter(xyYSim(1,:),xyYSim(2,:),5,(ssSim(2,:)+ssSim(3,:))-ssSim(5,:),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Lum-Mel';
+xlim([0,1]);
+ylim([0,1]);
+xlabel('x');
+
+%% plot as mel/lum 
+figure('defaultAxesFontSize',18)
+
+subplot(2,2,1)
+scatter(xyYCRTcombo(1,:),xyYCRTcombo(2,:),[],ssCRTcombo(5,:)./(ssCRTcombo(2,:)+ssCRTcombo(3,:)),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Mel/Lum';
+xlim([0,1]);
+ylim([0,1]);
+title('CRT');
+xlabel('x');
+ylabel('y');
+
+subplot(2,2,2)
+scatter(xyYLCDcombo(1,:),xyYLCDcombo(2,:),[],ssLCDcombo(5,:)./(ssLCDcombo(2,:)+ssLCDcombo(3,:)),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Mel/Lum';
+xlim([0,1]);
+ylim([0,1]);
+title('LCD');
+xlabel('x');
+ylabel('y');
+
+subplot(2,2,3)
+scatter(xyYDPcombo(1,:),xyYDPcombo(2,:),[],ssDPcombo(5,:)./(ssDPcombo(2,:)+ssDPcombo(3,:)),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Mel/Lum';
+xlim([0,1]);
+ylim([0,1]);
+title('Display++');
+xlabel('x');
+ylabel('y');
+
+subplot(2,2,4)
+scatter(xyYSim(1,:),xyYSim(2,:),5,ssSim(5,:)./(ssSim(2,:)+ssSim(3,:)),'filled');
+hold on;
+plot(xyYSL(1,idxSL),xyYSL(2,idxSL),'k-');
+c = colorbar;
+c.Label.String = 'Mel/Lum';
+xlim([0,1]);
+ylim([0,1]);
+xlabel('x');
+ylabel('y');
 
 %% plot on xy diagrams - normalisation question - priamries are in absolute radiance units but illuminants have been normalized
 
@@ -392,6 +500,7 @@ ylabel('y');
 subplot(2,2,2)
 scatter3(xyYSim(1,:),xyYSim(2,:),ssSim(2,:)+ssSim(3,:),'kx');
 hold on;
+plot3(xyYSL(1,idxSL),xyYSL(2,idxSL),ssSL(2,idxSL)+ssSL(3,idxSL),'r-');
 l(1) = plot3(xyYDP(1,idxSSDP),xyYDP(2,idxSSDP),ssDP(2,idxSSDP)+ssDP(3,idxSSDP),'b');
 l(2) = plot3(xyYCRT(1,idxSSCRT),xyYCRT(2,idxSSCRT),ssCRT(2,idxSSCRT)+ssCRT(3,idxSSCRT),'k');
 l(3) = plot3(xyYLCD(1,idxSSLCD),xyYLCD(2,idxSSLCD),ssLCD(2,idxSSLCD)+ssLCD(3,idxSSLCD),'Color',[0.75,0.75,0.75]);
@@ -403,6 +512,7 @@ legend(l,{'DP','CRT','LCD'});
 subplot(2,2,3)
 scatter3(xyYSim(1,:),xyYSim(2,:),ssSim(5,:),'kx');
 hold on;
+plot3(xyYSL(1,idxSL),xyYSL(2,idxSL),ssSL(2,idxSL)+ssSL(3,idxSL),'r-');
 l(1) = plot3(xyYDP(1,idxSSDP),xyYDP(2,idxSSDP),ssDP(5,idxSSDP),'b');
 l(2) = plot3(xyYCRT(1,idxSSCRT),xyYCRT(2,idxSSCRT),ssCRT(5,idxSSCRT),'k');
 l(3) = plot3(xyYLCD(1,idxSSLCD),xyYLCD(2,idxSSLCD),ssLCD(5,idxSSLCD),'Color',[0.75,0.75,0.75]);
@@ -414,6 +524,7 @@ legend(l,{'DP','CRT','LCD'});
 subplot(2,2,4)
 scatter3(xyYSim(1,:),xyYSim(2,:),ssSim(4,:),'kx');
 hold on;
+plot3(xyYSL(1,idxSL),xyYSL(2,idxSL),ssSL(2,idxSL)+ssSL(3,idxSL),'r-');
 l(1) = plot3(xyYDP(1,idxSSDP),xyYDP(2,idxSSDP),ssDP(4,idxSSDP),'b');
 l(2) = plot3(xyYCRT(1,idxSSCRT),xyYCRT(2,idxSSCRT),ssCRT(4,idxSSCRT),'k');
 l(3) = plot3(xyYLCD(1,idxSSLCD),xyYLCD(2,idxSSLCD),ssLCD(4,idxSSLCD),'Color',[0.75,0.75,0.75]);
@@ -422,65 +533,57 @@ ylabel('y');
 zlabel('Rod');
 legend(l,{'DP','CRT','LCD'});
 
-%% heatmaps of simulated responses
-
-figure('defaultAxesFontSize',18)
-
-subplot(1,3,1)
-scatter(xyYSim(1,:),xyYSim(2,:),5,(ssSim(2,:)+ssSim(3,:)),'filled');
-c = colorbar
-c.Label.String = 'Lum';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-ylabel('y');
-
-
-subplot(1,3,2)
-scatter(xyYSim(1,:),xyYSim(2,:),5,(ssSim(4,:)),'filled');
-c = colorbar
-c.Label.String = 'Rod';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-ylabel('y');
-
-
-subplot(1,3,3)
-scatter(xyYSim(1,:),xyYSim(2,:),5,(ssSim(5,:)),'filled');
-c = colorbar
-c.Label.String = 'Mel';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-ylabel('y');
-
-%% difference of mel and lum heatmap
-figure('defaultAxesFontSize',18)
-subplot(1,2,1)
-scatter(xyYSim(1,:),xyYSim(2,:),5,(ssSim(2,:)+ssSim(3,:))-ssSim(5,:),'filled');
-c = colorbar
-c.Label.String = 'Lum-Mel';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-
-subplot(1,2,2)
-scatter(xyYSim(1,:),xyYSim(2,:),5,ssSim(5,:)./(ssSim(2,:)+ssSim(3,:)),'filled');
-c = colorbar
-c.Label.String = 'Mel/Lum';
-xlim([0,1]);
-ylim([0,1]);
-xlabel('x');
-ylabel('y');
-
-% plot these on display heatmaps
 % difference of simulated and display
-% plot L+M vs mel
 % add in spectral locus and daylight locus to all above plots
 % move to MacLeod Boynton space with displays
 
 %% correlations
 
 figure('defaultAxesFontSize',18)
+subplot(3,2,1)
 scatter((ssSim(2,:)+ssSim(3,:)),ssSim(5,:),'k.');
+% hold on;
+% h(1)=plot(ssCRT(2,idxCRT)+ssCRT(3,idxCRT),ssCRT(5,idxCRT),'r-','LineWidth',2);
+% h(2)=plot(ssDP(2,idxCRT)+ssDP(3,idxCRT),ssDP(5,idxCRT),'b-','LineWidth',2);
+% h(3)=plot(ssLCD(2,idxCRT)+ssLCD(3,idxCRT),ssLCD(5,idxCRT),'-','Color',[0.85,0.85,0.85],'LineWidth',2);
+% legend(h,{'CRT','DP','LCD'});
+xlabel('L+M')
+ylabel('Mel')
+
+subplot(3,2,2)
+scatter((ssSim(2,:)+ssSim(3,:)),ssSim(4,:),'k.');
+xlabel('L+M')
+ylabel('Rod')
+
+subplot(3,2,3)
+scatter(ssSim(4,:),ssSim(5,:),'k.');
+xlabel('Rod')
+ylabel('Mel')
+
+subplot(3,2,4)
+scatter(ssSim(3,:)./(ssSim(2,:)+ssSim(3,:)),ssSim(1,:)./(ssSim(2,:)+ssSim(3,:)),'k.');
+xlabel('L/L+M')
+ylabel('S/L+M')
+
+subplot(3,2,5)
+scatter(ssSim(3,:)./(ssSim(2,:)+ssSim(3,:)),ssSim(5,:)./(ssSim(2,:)+ssSim(3,:)),'k.');
+xlabel('L/L+M')
+ylabel('I/L+M')
+
+subplot(3,2,6)
+scatter(ssSim(1,:)./(ssSim(2,:)+ssSim(3,:)),ssSim(5,:)./(ssSim(2,:)+ssSim(3,:)),'k.');
+xlabel('S/L+M')
+ylabel('I/L+M')
+
+%% plot discrimination histograms for L+M vs Mel
+figure()
+h(1)=histogram(ssSim(5,:)./(ssSim(2,:)+ssSim(3,:)),'FaceColor',[0,0,0]);
+hold on;
+h(2)=histogram(ssCRTcombo(5,:)./(ssCRTcombo(2,:)+ssCRTcombo(3,:)),'FaceColor',[1,0,0]);
+h(3)=histogram(ssLCDcombo(5,:)./(ssLCDcombo(2,:)+ssLCDcombo(3,:)),'FaceColor',[0.75,0.75,0.75]);
+h(4)=histogram(ssDPcombo(5,:)./(ssDPcombo(2,:)+ssDPcombo(3,:)),'FaceColor',[0,0,1]);
+legend(h,{'Sim','CRT','LCD','DP'})
+
+%% normalizing display gamuts
+%% plot in DKL space with mel axis instead of lum axis and plot display gamuts
+%% plot in MB space with extended mel axis and always plot displays
